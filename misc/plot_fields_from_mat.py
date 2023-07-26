72c7490b852ea9198b9477406d39f09b86a1590e
Edward Hughes McDugald <emcdugald@pn2305447.lanl.gov>
2023-07-26 11:51:26 -0600

    removing old plotter

--- misc/plot_fields_from_mat.py
+++ misc/plot_fields_from_mat.py
@@ -15,30 +15,18 @@ for i in range(len(data_dir)-1):
     fidx = i
     fname = data_dir[fidx]
     data = sio.loadmat(proj+"/matData"+"/"+fname)
-    # if "agg" in fname:
-    #     continue
 
-    if 'struct' in fname and 'unstruct' not in fname:
-        lons = (data['longitude'])*180./np.pi
-        lats = (data['latitude'])*180./np.pi
-        lons = lons.flatten()
-        lats = lats.flatten()
-    else:
-        lons = (data['longitude'][0,:])*180./np.pi
-        lats = (data['latitude'][0,:])*180./np.pi
+    longitude = data['longitude']
+    latitude = data['latitude']
 
-    lons = np.where(180<lons,lons-360.,lons)
     z = data['zt']
-    sensor_locs = data['sensor_locs'].T
 
     if 'struct' in fname and 'unstruct' not in fname:
-        sensor_lons = sensor_locs[1] * 180. / np.pi
-        sensor_lons = np.where(180 < sensor_lons, sensor_lons - 360., sensor_lons)
-        sensor_lats = sensor_locs[0] * 180. / np.pi
+        sensor_lons = data['sensor_locs'][:,1]
+        sensor_lats = data['sensor_locs'][:,0]
     else:
-        sensor_lons = sensor_locs[0]*180./np.pi
-        sensor_lons = np.where(180 < sensor_lons, sensor_lons - 360., sensor_lons)
-        sensor_lats = sensor_locs[1]*180./np.pi
+        sensor_lons = data['sensor_locs'][:,0]
+        sensor_lats = data['sensor_locs'][:,1]
 
     if "agg" in fname:
         times = np.arange(0,len(z),1).tolist()
@@ -46,20 +34,17 @@ for i in range(len(data_dir)-1):
     else:
         times= data['t']
         sampled_times = range(len(times[0]))[::round(len(times[0]) / 6)]
-    #fig, axs = plt.subplots(nrows=3, ncols=2,subplot_kw={'projection': ccrs.PlateCarree()})
     fig, axs = plt.subplots(nrows=3, ncols=2)
     for ax,t in zip(axs.flat, sampled_times):
-        #ax.coastlines(resolution='50m', color='black', linewidth=1)
-        #field = ax.tricontourf(lons, lats, z[t][:,0], cmap='bwr', alpha=.5, transform=ccrs.PlateCarree())
         if 'struct' in fname and 'unstruct' not in fname:
-            zplt = z[t].flatten()
+            zplt = np.where((data['ismask']==0),z[t,:,:,0],1)
+            field = ax.scatter(longitude, latitude, c=zplt, cmap='bwr')
         else:
-            zplt = z[t][:,0]
-        field = ax.scatter(lons, lats, c=zplt, cmap='bwr')
-        ax.scatter(sensor_lons, sensor_lats,color='k',s=.1)
+            zplt = np.where((data['ismask']==0),z[t,:,0],1)
+            field = ax.scatter(longitude, latitude, c=zplt, cmap='bwr',s=.2)
+        ax.scatter(sensor_lons, sensor_lats,color='k',s=.5)
         ax.set_title("t="+str(t))
         plt.colorbar(field,ax=ax)
-    #fig.colorbar(field,ax=axs.ravel().tolist(),location='right')
     fig.set_figheight(int(3*(2+1)))
     fig.set_figwidth(int(2*(4+1)))
     plt.tight_layout()
