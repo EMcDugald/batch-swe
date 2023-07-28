8128208d9e36d04d1dcbf71ee89fb917b1a323fa
Edward Hughes McDugald <emcdugald@pn2305447.lanl.gov>
2023-07-28 12:14:19 -0600

    plots should default to showing z

--- misc/plot_fields_from_mat.py
+++ misc/plot_fields_from_mat.py
@@ -11,10 +11,10 @@ filterwarnings(action='ignore', category=DeprecationWarning, message='`np.bool` 
 proj = os.path.normpath(os.getcwd() + os.sep + os.pardir)
 data_dir = os.listdir(proj+"/matData")
 data_dir.sort()
-# var = 'zt'
-# mask_val = 1
-var = 'du'
-mask_val = 1e-3
+var = 'zt'
+mask_val = 1
+# var = 'du'
+# mask_val = 1e-3
 for i in range(len(data_dir)-1):
     fidx = i
     fname = data_dir[fidx]
