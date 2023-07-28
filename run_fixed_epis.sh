88446c51d2903f1e96a7e4ced5f5a3238041ac4d
Edward Hughes McDugald <emcdugald@pn2305447.lanl.gov>
2023-07-28 16:16:30 -0600

    more figs, new script

--- run_fixed_epis.sh
+++ run_fixed_epis.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-lon_arr=("179.3502" "323.9859" "185.5075" )
-lat_arr=("-18.4743" "-74.5754" "-15.6278")
+lon_arr=("166.5160" )
+lat_arr=("-13.9660")
 
-num_runs=${#lon_arr[@]}
-echo "num_runs: $num_runs"
 mesh_file="cdfData/mesh_w_elev_cvt_7.nc"
 init_file="cdfData/initial_condition.nc"
 delete_nc=$1 #pass 1 if you want to save .nc files, 0 otherwise
+wait_until_first_detection=$2 #passing 1 suppresses frames until a signal above 1e-3 is discovered
+suppress_zero_sigs=$3 #passing 1 eliminates sensor locations that never exceed 1e-3
+regrid=$4 #passing 1 interpolates the data onto a strucured grid
+subsample_fctr=$5 #passing n subsamples the output by n, ie arr[::n] or arr[::n,::n]
+num_times=$6 #passing n randomly selects n time slices to keep. passing 0 keeps all times
+agg_data=$7 #passing 1 aggregates all the data from the run
+with_div=$8 #passing 1 includes velocity divergence
+lon_arr=("166.5160" )
+lat_arr=("-13.9660")
+num_runs=${#lon_arr[@]}
+echo "epicenter sampling indices: ${ids[@]}"
 
 
 for ((i=1; i<=$num_runs; i++))
@@ -30,18 +39,11 @@ do
     --wave-xmid=$epi_long \
     --wave-ymid=$epi_lat
 
-    break_flag=$(python is_too_shallow.py $epi_long $epi_lat)
-    if [ "$break_flag" = "True" ]; then
-      rm $init_file
-      echo "water too shallow at $epi_long, $epi_lat"
-      continue
-    fi
-
     # Run swe.py on the initial condition. This will generate a .mat file identified with the epicenter and num_run
     printf -v i_pad "%03d" $i
     python solver/swe.py --mpas-file=$init_file \
-     --time-step=216. \
-     --num-steps=5 \
+     --time-step=54. \
+     --num-steps=800 \
      --save-freq=1 \
      --stat-freq=100 \
      --loglaw-z0=0.0025 \
@@ -55,12 +57,16 @@ do
     out_nc="cdfData/"$i_pad"_"$epi_long"_"$epi_lat".nc"
     echo "out nc: $out_nc"
 
-    python make_mat.py $i_pad $epi_long $epi_lat
+    python make_mat.py $i_pad $epi_long $epi_lat $wait_until_first_detection $suppress_zero_sigs $regrid $subsample_fctr $num_times $agg_data $with_div
 
     # Delete initial condition file before next run
     rm $init_file
     # Delete nc file if desired
-    if [ "$delete_nc" = 0 ]; then
+    if [ "$delete_nc" = 1 ]; then
         rm $out_nc
     fi
 done
+
+if [ "$agg_data" = 1 ]; then
+  python agg_mats.py $regrid $subsample_fctr $num_times $num_runs $with_div
+fi
